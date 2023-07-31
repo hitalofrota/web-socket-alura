64eafe80e495955df470dbb91eed7ddc6883d03f
HITALO VINICIUS RODRIGUES FROTA <hitalo.frota@tallos.intra.br>
2023-07-31 14:54:19 -0300

    feature/roms finished

--- src/socket-back.js
+++ src/socket-back.js
@@ -2,9 +2,13 @@ import io from "./servidor.js";
 
 io.on("connection", (socket) => {
     console.log(`Um cliete se conectou! ID: ${socket.id}`)
+    
+    socket.on("selecionar_documento", (nomeDocumento) => {
+        socket.join(nomeDocumento)
+    })
 
-    socket.on("texto_editor", (texto) => {
-        socket.broadcast.emit("texto_editor_clientes", texto)
+    socket.on("texto_editor", ({ texto, nomeDocumento }) => {
+        socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
     })
 })
 
